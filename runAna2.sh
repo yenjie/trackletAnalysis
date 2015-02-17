53bfec682f9ff5981d634a0c0c6ca027cd9e666e
Yen-Jie Lee <yenjie@mit.edu>
2015-02-17 02:54:57 +0100

    pp 7 TeV code

---
+++ runAna2.sh
@@ -0,0 +1,23 @@
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 0 10
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 10 20
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 20 30
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 30 40
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 40 50
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 50 60
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 60 70
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 70 80
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 80 90
+./anaData2.sh newUltimateSample/TrackletTree-Run132440-hfcuts-new.root Run132440 test 90 100
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ runCor.sh
@@ -0,0 +1,3 @@
+./getCorrection.sh sample-7000GeV/TrackletTree-D6T-addbgM2.root D6T-7TeV-HF1-addbgM2 0 1000
+./getCorrection.sh sample-7000GeV/TrackletTree-D6T-7TeV.root D6T-7TeV-HF1 0 1000
+./getCorrection.sh sample-7000GeV/TrackletTree-D6T-TrackletVtx.root D6T-7TeV-HF1-trackletVtx 0 1000
