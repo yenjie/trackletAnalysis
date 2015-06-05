cfdda83128d2b76e8fddaaedb284b3e961afbd04
Yen-Jie Lee <yenjie@mit.edu>
2015-06-05 23:43:20 +0200

    fix sim vertex

--- cmssw/MitHig/PixelTrackletAnalyzer/src/StripHitAnalyzer.cc
+++ cmssw/MitHig/PixelTrackletAnalyzer/src/StripHitAnalyzer.cc
@@ -462,11 +462,12 @@ StripHitAnalyzer::fillVertices(const edm::Event& iEvent){
         // for (unsigned int iv=0; iv<SimVtx->size(); iv++) {
          for (unsigned int iv=0; iv<=0; iv++) {
 	    //itr->momentum();
+            if (fabs(simVtxItr->position().Z())<30) {
+   	          pev_.vx[0]=simVtxItr->position().X();
+                  pev_.vy[0]=simVtxItr->position().Y();
+                  pev_.vz[0]=simVtxItr->position().Z();
+            }//cout <<pev_.vx[0]<<" "<<pev_.vy[0]<<" "<<pev_.vz[0]<<" "<<simVtxItr->position().T()<<endl;
 	    simVtxItr++;
-            pev_.vx[0]=simVtxItr->position().X();
-            pev_.vy[0]=simVtxItr->position().Y();
-            pev_.vz[0]=simVtxItr->position().Z();
-            //cout <<pev_.vx[0]<<" "<<pev_.vy[0]<<" "<<pev_.vz[0]<<" "<<simVtxItr->position().T()<<endl;
 	 }
 	 //cout <<"============"<<endl;
       }
