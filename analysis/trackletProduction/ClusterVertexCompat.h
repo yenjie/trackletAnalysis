dd0a61316bba48d2fc631cdb50addf36c475b39c
Marta <marta.verweij@cern.ch>
2015-06-14 14:42:52 +0200

    add option to use layer 1 or 2

--- analysis/trackletProduction/ClusterVertexCompat.h
+++ analysis/trackletProduction/ClusterVertexCompat.h
@@ -11,20 +11,24 @@
 
 #include "RecoHit.h"
 
-Int_t getContainedHits(const std::vector<RecoHit> &hits, double z0, double &diff);
+Int_t getContainedHits(const std::vector<RecoHit> &hits, double z0, double &diff, double slope = 0.43);
 
-Double_t getClusVtxCompat(const std::vector<RecoHit> &hits) {
+Double_t getClusVtxCompat(const std::vector<RecoHit> &hits, const Int_t layer = 1) {
 
   const Double_t   minZ_ = -30.;          // beginning z-vertex position
   const Double_t   maxZ_ = 30.;           // end z-vertex position
   const Double_t   zStep_ = 0.01;         // size of steps in z-vertex test
+
+  Double_t slope = 0.43;
+  if(layer==1)      slope = 0.43;
+  else if(layer==2) slope = 0.25;
 
   // estimate z-position from cluster lengths
   Double_t zest = 0.;
   Int_t nhits = 0, nhits_max = 0;
   Double_t diff = 0, diff_max = 1e+9;
   for(Double_t z0 = minZ_; z0 <= maxZ_; z0 += zStep_) {
-    nhits = getContainedHits(hits, z0, diff);
+    nhits = getContainedHits(hits, z0, diff, slope);
     if(nhits == 0)
       continue;
     if(nhits > nhits_max) {
@@ -39,9 +43,9 @@ Double_t getClusVtxCompat(const std::vector<RecoHit> &hits) {
 
   diff = 0;
   int nbest=0, nminus=0, nplus=0;
-  nbest = getContainedHits(hits,zest,diff);
-  nminus = getContainedHits(hits,zest-10.,diff);
-  nplus = getContainedHits(hits,zest+10.,diff);
+  nbest = getContainedHits(hits,zest,diff,slope);
+  nminus = getContainedHits(hits,zest-10.,diff,slope);
+  nplus = getContainedHits(hits,zest+10.,diff,slope);
   
   Double_t clusVtxQual=0.0;
   if ((nminus+nplus)> 0)
@@ -52,14 +56,14 @@ Double_t getClusVtxCompat(const std::vector<RecoHit> &hits) {
   return clusVtxQual;
 }
 
-Int_t getContainedHits(const std::vector<RecoHit> &hits, double z0, double &diff) {
+Int_t getContainedHits(const std::vector<RecoHit> &hits, double z0, double &diff, double slope) {
   // Calculate number of hits contained in v-shaped window in cluster y-width vs. z-position.
   Int_t n = 0;
   diff   = 0.;
      
   for(std::vector<RecoHit>::const_iterator hit = hits.begin(); hit!= hits.end(); ++hit) {
     Double_t z = hit->r/tan(2.*atan(exp(-hit->eta)));
-    Double_t exp = 1.5+0.43*fabs(z-z0);
+    Double_t exp = 1.5+slope*fabs(z-z0);
     if(fabs(exp - hit->cs) <= 1.) { //v-shape with 1 cm width
       diff += fabs(exp - hit->cs);
       n++;
